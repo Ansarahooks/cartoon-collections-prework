0f7db59148f89064fb20ddc05998adfebd475b29
ansara.hooks <ansara.hooks@gmail.com>
2019-03-04 19:26:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarfs)
-  dwarfs.each_with_index {|name, index| print "#{index + 1}. #{name}"}
+  dwarfs.each_with_index {|name, index| print "#{index + 1}.#{name}"}
 end
 
 def summon_captain_planet(planeteer_calls)
